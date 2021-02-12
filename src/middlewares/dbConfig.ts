d66221d99b555fa773f6515a6334f0e3e02edcc8
mirkotorrisi <mirko.torrisi92@gmail.com>
2021-02-12 22:28:21 +0100

    change db

--- src/middlewares/dbConfig.ts
+++ src/middlewares/dbConfig.ts
@@ -3,11 +3,11 @@ import config from "config";
 const db = async () => {
   const pool = await mysql.createPool({
     connectionLimit: 10,
-    host: "localhost", //"sql7.freemysqlhosting.net",
+    host: "sql307.epizy.com", //"sql7.freemysqlhosting.net",
     port: 3306,
-    user: "root", //config.get("db_username"),
-    password: "", //config.get("db_password"),
-    database: "covid19", //config.get("db_username"),
+    user: "epiz_27917454", //config.get("db_username"),
+    password: "RAxZHjnAJlK", //config.get("db_password"),
+    database: "epiz_27917454_covid19", //config.get("db_username"),
   });
   return await pool.getConnection();
 };
